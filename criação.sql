612f1b9c343818be62f64b15f23dda91190702ef
slordian <arthur.silva.2@acdemico.ifpb.edu.br>
2022-11-05 23:49:20 -0300

    update

--- criação.sql
+++ criação.sql
@@ -22,3 +22,37 @@ CREATE TABLE candidato (
 	telefone1 INT NOT NULL,
 	telefone2 INT
 );
+
+CREATE TABLE empregado (
+	matrícula INT PRIMARY KEY,
+	nome VARCHAR(67) NOT NULL,
+	data_de_nascimento DATE NOT NULL,
+	salário NUMERIC(8,2) NOT NULL CHECK (salário > 0),
+	sexo CHAR(1) CHECK (sexo in ('M', 'F')),
+	uf CHAR(2) NOT NULL,
+	rua VARCHAR(67),
+	número INT,
+	id_departamento INT REFERENCES departamento(id_departamento) NOT NULL	
+);
+
+CREATE TABLE "gratifição" (
+	código INT PRIMARY KEY CHECK (código > 0),
+	atividade VARCHAR(67),
+	valor NUMERIC(8,2) CHECK (valor > 0)
+);
+
+CREATE TABLE "projeto_gratificação" (
+	matrícula INT REFERENCES empregado(matrícula),
+	id_projeto INT REFERENCES projeto(id_projeto),
+	horas_trabalhadas INT NOT NULL CHECK (horas_trabalhadas >= 0),
+	código_gratificação INT REFERENCES "gratifição"(código),
+	PRIMARY KEY (matrícula, id_projeto)
+);
+
+CREATE TABLE projeto_candidato (
+	id_projeto INT REFERENCES projeto(id_projeto),
+	cpf INT REFERENCES candidato(cpf),
+	data_entrevista DATE,
+	contratação BOOLEAN NOT NULL,
+	PRIMARY KEY (id_projeto, cpf)
+);
