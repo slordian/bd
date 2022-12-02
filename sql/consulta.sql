1653834bb42c8ddb417a277f4e790c3cc304e19d
slordian <arthur.silva.2@acdemico.ifpb.edu.br>
2022-12-02 19:56:50 -0300

    update

--- sql/consulta.sql
+++ sql/consulta.sql
@@ -32,3 +32,44 @@ SELECT * FROM projeto ORDER BY orçamento ASC;
 
 -----------------------------------------------------------------
 -----------------------------------------------------------------
+
+-- Enunciado: Mostre quantos projetos existem por departamento
+
+SELECT id_departamento, COUNT(id_projeto)
+FROM projeto GROUP BY id_departamento
+ORDER BY id_departamento;
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
+-- Enunciado: Mostre os maiores e menores salários por sexo
+
+SELECT sexo, MAX(salário) AS "maior salário", MIN(salário) AS "menor salário"
+FROM empregado
+GROUP BY sexo;
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
+-- Enunciado: Mostre a soma de todas as horas trabalhadas em projetos de recrutamento
+
+SELECT SUM(horas_trabalhadas)
+FROM recrutamento_empregado;
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
+-- Enunciado: Mostre a média de salários pretendidos pelos candidatos
+
+SELECT CAST(AVG(salário_pretendido) AS NUMERIC(8, 2)) AS "média de salários pretendidos"
+FROM candidato;
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
+-- Enunciado: Mostre 
+
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
