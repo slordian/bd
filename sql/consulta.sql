e63bdfb40210b1672df4b0d3c9ec4786785c865e
slordian <arthur.silva.2@acdemico.ifpb.edu.br>
2022-12-05 21:12:01 -0300

    update consulta.sql

--- sql/consulta.sql
+++ sql/consulta.sql
@@ -1,39 +1,39 @@
--- Enunciado: Mostre os empregados que têm a matrícula entre 0 e 50
+-- Enunciado: Mostre os empregados que têm a matrícula entre 0 e 50.
 
 SELECT * FROM empregado WHERE matrícula BETWEEN 0 AND 50;
 
 -----------------------------------------------------------------
 -----------------------------------------------------------------
 
--- Enunciado: Mostre os candidatos que não têm um cargo pretendido
+-- Enunciado: Mostre os candidatos que não têm um cargo pretendido.
 
 SELECT * FROM candidato WHERE cargo_pretendido IS NULL;
 
 -----------------------------------------------------------------
 -----------------------------------------------------------------
 
--- Enunciado: Mostre as informações dos empregados que moram em São Paulo, Santa Catarina e Rondônia
+-- Enunciado: Mostre as informações dos empregados que moram em São Paulo, Santa Catarina e Rondônia.
 
 SELECT * FROM empregado WHERE uf IN ('SP', 'SC', 'RO');
 
 -----------------------------------------------------------------
 -----------------------------------------------------------------
 
--- Enunciado: Mostre as linhas da tabela de gratificações onde a atividade contiver Ajudante
+-- Enunciado: Mostre as linhas da tabela de gratificações onde a atividade contiver Ajudante.
 
 SELECT * FROM gratificacao WHERE atividade LIKE 'Ajudante %';
 
 -----------------------------------------------------------------
 -----------------------------------------------------------------
 
--- Enunciado: Mostre a tabela dos projetos da linha com menor orçamento para a linha com maior orçamento
+-- Enunciado: Mostre a tabela dos projetos da linha com menor orçamento para a linha com maior orçamento.
 
 SELECT * FROM projeto ORDER BY orçamento ASC;
 
 -----------------------------------------------------------------
 -----------------------------------------------------------------
 
--- Enunciado: Mostre quantos projetos existem por departamento
+-- Enunciado: Mostre quantos projetos existem por departamento.
 
 SELECT id_departamento, COUNT(id_projeto)
 FROM projeto GROUP BY id_departamento
@@ -42,7 +42,7 @@ ORDER BY id_departamento;
 -----------------------------------------------------------------
 -----------------------------------------------------------------
 
--- Enunciado: Mostre os maiores e menores salários por sexo
+-- Enunciado: Mostre os maiores e menores salários por sexo.
 
 SELECT sexo, MAX(salário) AS "maior salário", MIN(salário) AS "menor salário"
 FROM empregado
@@ -51,7 +51,7 @@ GROUP BY sexo;
 -----------------------------------------------------------------
 -----------------------------------------------------------------
 
--- Enunciado: Mostre a soma de todas as horas trabalhadas em projetos de recrutamento
+-- Enunciado: Mostre a soma de todas as horas trabalhadas em projetos de recrutamento.
 
 SELECT SUM(horas_trabalhadas)
 FROM recrutamento_empregado;
@@ -59,7 +59,7 @@ FROM recrutamento_empregado;
 -----------------------------------------------------------------
 -----------------------------------------------------------------
 
--- Enunciado: Mostre a média de salários pretendidos pelos candidatos
+-- Enunciado: Mostre a média de salários pretendidos pelos candidatos.
 
 SELECT CAST(AVG(salário_pretendido) AS NUMERIC(8, 2)) AS "média de salários pretendidos"
 FROM candidato;
@@ -67,8 +67,20 @@ FROM candidato;
 -----------------------------------------------------------------
 -----------------------------------------------------------------
 
--- Enunciado: Mostre 
+-- Enunciado: Mostre o nome do empregado, as horas trabalhadas, o nome do projeto em que trabalhou e a gratificação que recebeu.
 
+SELECT e.nome, p.nome AS "projeto", re.horas_trabalhadas, g.valor
+FROM empregado e, recrutamento_empregado re, projeto p, gratificacao g
+WHERE e.matrícula = re.matrícula AND re.id_projeto = p.id_projeto AND g.código = re.código_gratificação;
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
+-- Enunciado: Mostre o nome do empregado eo departamento em que trabalha.
+
+SELECT e.nome, d.nome
+FROM empregado e JOIN departamento d
+ON (e.id_departamento = d.id_departamento);
 
 -----------------------------------------------------------------
 -----------------------------------------------------------------
